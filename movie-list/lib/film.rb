e8287efa1eb4b0284a95c71b75436d1629729591
Aydin Doyak <aydin.doyak@pardus.org.tr>
2015-09-01 17:16:15 +0300

    add colorizing and prompts and reviews

--- movie-list/lib/film.rb
+++ movie-list/lib/film.rb
@@ -1,10 +1,12 @@
 #!/usr/bin/env ruby
 
+require 'colorize'
+
 module Film
   def Film.add(title, rating)
     movie = Movie.create(title: title, rating: rating)
     if movie.errors.any?
-      movie.errors.messages
+      puts "Movie can't be blank or already exists.".colorize(:red)
     else
       "#{title} added to your movie list with your rating #{rating}"
     end
@@ -12,44 +14,36 @@ module Film
 
   def Film.display
     Movie.all.order(:title).each do |movie|
-      printf "%-70s : %s\n", movie.title, movie.rating
+      printf "%-70s : %s\n".colorize(:light_blue), movie.title, movie.rating
     end
   end
 
   def Film.delete(title)
-    unless movie = Movie.find_by(title: title)
-      puts "Movie does not exist"
-    else
       movie = Movie.find_by(title: title)
-      movie.destroy
-      if movie.errors.any?
-        movie.errors.messages
+      if movie == nil or movie.errors.any?
+        puts "#{title} does not exist in the movie list".colorize(:red)
       else
-        puts "#{title} deleted from your movies"
+        movie.delete
+        puts "#{title} deleted from your movies".colorize(:green)
       end
-    end
   end
 
   def Film.update(title, rating)
-    unless movie = Movie.find_by(title: title)
-      puts "Movie does not exist"
-    else
       movie = Movie.find_by(title: title)
-      movie.update_attributes(rating: rating)
-      if movie.errors.any?
-        movie.errors.messages
+      if movie == nil or movie.errors.any?
+        puts "#{title} does not exist in the movie list".colorize(:red)
       else
-        puts "#{title} updated with your new rating #{rating}"
+        movie.update_attributes(rating: rating)
+        puts "#{title} updated with your new rating #{rating}".colorize(:green)
       end
-    end
   end
 
   def Film.help
-    puts "add: Add a movie to your movie list with a rating between 1-10 
+    puts "\nadd: Add a movie to your movie list with a rating between 1-10 
 update: Type down an existing movie title and update its rating in your movie list.
 delete: Type down an existing movie title and delete that entry from your movie list.
 display: See your Movie List with the ratings.
 quit: Exit Movie List (Quit,quit,q,Q options should work!)
-help: See this guide."
+help: See this guide.\n".colorize(:color => :green)
   end
 end
--- movie-list/main.rb
+++ movie-list/main.rb
@@ -2,48 +2,50 @@
 
 require './db/schema'
 require './lib/film'
+require 'colorize'
 
 def main
   quit_options = ['quit', 'Quit', 'Q', 'q']
-  puts "What do you want to do with your Movie List?\nType help for instructions"
+  prompt = "> ".colorize(:light_blue)
+  print "What do you want to do with your Movie List?\nType help for instructions\n".colorize(:green), prompt
   choice = gets.chomp
 
   unless quit_options.include?(choice)
     case choice
       when 'add'
-        puts "Give a movie name to add:"
+        print "Give a movie name to add:\n", prompt
         title = gets.chomp
-        puts "Rate this movie objectively from 1 to 10:"
+        print "Rate this movie objectively from 1 to 10:\n", prompt
         rating = gets.chomp
         puts Film.add(title, rating)
         main
       when 'delete'
-        puts "Give a movie name to delete:"
+        print "Give a movie name to delete:\n", prompt 
         title = gets.chomp
         puts Film.delete(title)
         main
       when 'update'
-        puts "Give a movie name to update:"
+        print "Give a movie name to update:\n", prompt
         title = gets.chomp
-        puts "Give the new value of your rating:"
+        print "Give the new value of your rating:\n", prompt
         rating = gets.chomp
         puts Film.update(title, rating)
         main
       when 'display'
         movie_header = "Movies"
         movie_rating_header = "Rating"
-        printf "%-70s %s\n\n", movie_header, movie_rating_header
+        printf "%-70s %s\n\n".colorize(:color => :bold, :background => :blue), movie_header, movie_rating_header
         Film.display
         main
       when 'help'
         Film.help
         main
       else
-        puts "Unrecognized choice. Type 'help' for options."
+        print "Unrecognized choice. Type 'help' for options.\n".colorize(:red), prompt
         main
     end
   else
-    puts "Thank you for using the Movie List!"
+    puts "Thank you for using the Movie List!".colorize(:green)
     exit 0
   end
 end
